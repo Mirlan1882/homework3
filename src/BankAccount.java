68b7a6ff4d25ae70fc61eafd7b716e2bc2ba7b09
Mirlan1882 <albaesenzhanov1002@gmail.com>
2022-08-13 17:14:43 +0600

    Initial commit

---
+++ src/BankAccount.java
@@ -0,0 +1,20 @@
+public class BankAccount {
+    double amount;
+    public double getAmount(){
+        return 0;
+
+    }
+    public void deposit(double sum) {
+        amount += sum;
+        {
+            System.out.println("мы пополнили счет:" + sum);
+        }
+    }
+        public void withDraw (int sum) throws LimitException {
+            if (amount < sum) { throw new LimitException ("Операция не доступно потому что запрашиваемая сумма больше чем счет", amount);
+            } else{
+                amount -= sum;
+                System.out.println("Вы сняли со счета:"+ sum);
+        }
+        }
+    }
---
+++ src/LimitException.java
@@ -0,0 +1,8 @@
+public class LimitException extends Exception{
+    public LimitException(String message, double remainingAmount) {
+        super(message);
+    }
+    public int getRemainingAmount(){
+        return 0;
+    }
+}
